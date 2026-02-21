44db8e831a4458ae94a36835112e877ba7f12c86
zainabattiq <zainabattiq54@gmail.com>
2026-02-21 15:57:18 +0500

    Add Chapter4-PS

---
+++ CHAPTER4/02-list-methods.py
@@ -0,0 +1,14 @@
+friends = ["Apples", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+print(friends)
+friends.append("Harry")
+print(friends)
+
+l1 = [1, 3, 63, 2, 6, 11]
+# l1.sort()
+# l1.reverse()
+# l1.insert(3, 333333) # Insert 333333 such that its index in the list is 3
+# value = l1.pop(3)
+# print(value)
+l1.remove(63)
+print(l1)
+
---
+++ CHAPTER4/04-tuple-methods.py
@@ -0,0 +1,12 @@
+a = (1, 45, 342, 3424, False, 45, "Rohan", "Shivam")
+print(type(a))
+print(a)
+
+no = a.count(45)
+print(no)
+
+i = a.index(False)
+print(i)
+
+print(len(a))
+
