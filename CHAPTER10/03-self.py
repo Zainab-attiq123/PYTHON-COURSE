dbf84111ecf3aeaeb3be93255455cc3647cffb5a
zainabattiq <zainabattiq54@gmail.com>
2026-02-22 21:43:43 +0500

    Add Chapter10

---
+++ CHAPTER10/03-self.py
@@ -0,0 +1,15 @@
+class Employee:
+    language = "Python" # This is a class attribute
+    salary = 1200000
+
+    def getInfo(self):
+        print(f"The language is {self.language}. The salary is {self.salary}")
+
+    def greet(self):
+        print("Good Morning")
+
+harry = Employee()
+# harry.language = "JavaScript" # This is an instance attribute
+harry.getInfo()
+harry.greet()
+# Employee.getInfo(harry)
